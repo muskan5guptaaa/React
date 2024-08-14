683562d8b61f5d764b8165bf60d4761db3eeca06
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-08-15 00:10:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//let rootEl=document.getElementById('root');
+
+
+//let p1=document.createElement('a');
+//p1.innerHTML='muskan gupta';
+//rootEl.append(p1);
+
+
+
+
+//let rootEl=document.getElementById('root');
+//let 
---
+++ vite-app-01/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div>
+      <div>App</div>
+      {/* calling sort of */}
+      {/* <Cat /> */}
+      {/* <Random /> */}
+      {/* <Event /> */}
+      {/* <Prop name="samarth" age="20" /> */}
+      {/* <Person name="samarth" age="20" /> */}
+      <Counter />
+    </div>
+  )
+}
+
+export default App
