604a6e9071e9d457cf9ce60e973b94083ffa5a67
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-08-15 23:56:17 +0530

    initial commit

--- vite-app-01/src/App.jsx
+++ vite-app-01/src/App.jsx
@@ -1,19 +1,64 @@
-import React from 'react'
-
-import Counter from './components/Counter'
+import React, { useState } from 'react'
 
 function App() {
+
+  let arr = [
+    {
+      title:"surya namaste",
+      desc:"early morning kro"
+    },
+    {
+      title:"shaam namaste",
+      desc:"early morning kro"
+    },
+    {
+      title:"raat namaste",
+      desc:"early morning kro"
+    }
+  ]
+
+  let [arrayy , setArrayy] = useState(arr);
+
   return (
     <div>
-      <div>App</div>
-      {/* calling sort of */}
-      {/* <Cat /> */}
-      {/* <Random /> */}
-      {/* <Event /> */}
-      {/* <Prop name="samarth" age="20" /> */}
-      {/* <Person name="samarth" age="20" /> */}
-      <Counter />
+      <h1>TODO LIST</h1>
+      <Todo todos={arrayy}  />
+      <AddTodo arrayy={arrayy} setArrayy={setArrayy}  />
     </div>
+  )
+}
+
+function AddTodo({arrayy , setArrayy}){
+  let [inp1, setInp1] = useState("")
+  let [inp2, setInp2] = useState("")
+  function changeInp1(e){
+    setInp1(e.target.value)
+  }
+  function changeInp2(e){
+    setInp2(e.target.value)
+  }
+  function handleAdd(){
+    setArrayy([...arrayy , {title:inp1 , desc:inp2}])
+  }
+  return(
+    <div>
+      <input onChange={changeInp1} type="text" value={inp1} />
+      <input onChange={changeInp2} type="text" value={inp2} />
+      <button onClick={handleAdd}>Add todo</button>
+    </div>
+  )
+}
+
+function Todo({todos}){
+  return(
+    todos.map((item , index)=>{
+      return(
+        <div key={index} style={{border:"2px solid black" , margin:"5px" }}>
+          <h1>Title: {item.title} </h1>
+          <h3>Description: {item.desc} </h3>
+        </div>
+      )
+    })
   )
 }
 
